87954758b8122b17d3d526b17d2d5b731592ba8d
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2019-03-27 23:07:28 -0300

    Query 'pacientes?paciente=nomeDoPaciente' funcionando

--- src/main/java/br/com/codenation/hospital/resource/ProductResource.java
+++ src/main/java/br/com/codenation/hospital/resource/ProductResource.java
@@ -1,12 +1,17 @@
 package br.com.codenation.hospital.resource;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import br.com.codenation.hospital.domain.Product;
+import br.com.codenation.hospital.resource.util.URL;
 import br.com.codenation.hospital.services.ProductService;
 
 @RestController
@@ -16,9 +21,13 @@ public class ProductResource {
 	@Autowired
 	private ProductService service;
 	
-	@GetMapping("/v1/hospitais/{id}/estoque/{produto}")
-	public ResponseEntity<List<Product>> findProductById(){
-		List<Product> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+	@GetMapping("/estoque/{id}")
+	public ResponseEntity<Product> findProductById(@PathVariable String id){
+		return ResponseEntity.ok().body(service.findById(id));
+	}
+	
+	@GetMapping("/estoque") //estoque?produto=nomeDoProduto
+	public ResponseEntity<List<Product>> findProduct(@RequestParam(value="produto", defaultValue="") String description){
+		return ResponseEntity.ok().body(service.findByProductDescription(URL.decodeParam(description)));
 	}
 }
